fdbf8c6a4879197d3f8d9238d158c1c92342e5f2
GUEDDA Oussama <oussama.guedda@cheops.fr>
2020-03-29 10:02:32 +0200

    ajout du premier page avec quelques fonctionnalités

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" message="ya3tiik 3assba"/>
+    <HelloWorld msg="Welcome to Your 9o7b app" message="ya3tiik essa7a"/><br>
+    <cars name="peugeot" modele= "206" year="2023"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Cars from './components/Cars.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Cars
   }
 }
 </script>
---
+++ src/components/Cars.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul v-for="car in cars" :key="car.name">
+    <label> Name: <input type="texte" v-model="car.name"/></label>
+    <label> modele: <input type="texte" v-model="car.modele"/></label>
+    <label> year: <input type="texte" v-model="car.year"/></label><br>
+      <li>{{ car.name }}</li>
+      <li>{{ car.modele }}</li>
+      <li>{{ car.year }}</li>
+    </ul>
+    <button @click="addcar">Add car</button>
+    <p>Nombre de car : {{ counter }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ['name', 'modele', 'year'],
+  data() {
+    return {
+      cars: []
+    };
+  },
+  methods: {
+    addcar() {
+      this.cars.push({
+        name: this.name,
+        modele: this.modele,
+        year: this.year
+      });
+    }
+  },
+  computed: {
+    counter() {
+      return this.cars.length;
+    }
+  },
+  mounted: {}
+};
+</script>
+<style scoped>
+</style>
