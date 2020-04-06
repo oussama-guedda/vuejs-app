d2430c21e342ef9dc9832b67b3063431d72a6a97
GUEDDA Oussama <oussama.guedda@cheops.fr>
2020-04-06 08:49:14 +0200

    last change

--- src/components/Cars.vue
+++ src/components/Cars.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <ul v-for="car in cars" :key="car.name">
-    <label> Name: <input type="texte" v-model="car.name"/></label>
-    <label> modele: <input type="texte" v-model="car.modele"/></label>
-    <label> year: <input type="texte" v-model="car.year"/></label><br>
+      <label>
+        Name:
+        <input type="texte" v-model="car.name" />
+      </label>
+      <label>
+        modele:
+        <input type="texte" v-model="car.modele" />
+      </label>
+      <label>
+        year:
+        <input type="texte" v-model="car.year" />
+      </label>
+      <br />
       <li>{{ car.name }}</li>
       <li>{{ car.modele }}</li>
       <li>{{ car.year }}</li>
@@ -12,13 +22,21 @@
     <p>Nombre de car : {{ counter }}</p>
   </div>
 </template>
+
 <script>
+import Vue from "vue";
+import VueResource from "vue-resource";
+Vue.use(VueResource);
+
 export default {
-  props: ['name', 'modele', 'year'],
+  props: ["name", "modele", "year"],
   data() {
     return {
       cars: []
     };
+  },
+  http: {
+    root: "http://localhost:3000"
   },
   methods: {
     addcar() {
@@ -34,8 +52,20 @@ export default {
       return this.cars.length;
     }
   },
-  mounted: {}
+  mounted() {
+    this.$resource("cars")
+      .get()
+      .then(
+        response => {
+          this.cars = response.data
+        },
+        response => {
+          console.log('erreur',response)
+        }
+      );
+  }
 };
 </script>
+
 <style scoped>
 </style>
